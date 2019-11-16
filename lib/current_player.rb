7efd6966db13e26432beb4bd76d4027651fc4fcb
Michael Fahim <michael_fahim@yahoo.com>
2019-11-16 04:29:55 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -3,17 +3,10 @@ counter = 0
 board.each do |spot|
   if spot == "X" || spot == "O"
     counter +=1
-
-
   end
-
 end
-counter
 end
 
 def current_player(board)
 turn_count(board) % 2 == 0 ? "X" : "O"
-
-
-
 end
